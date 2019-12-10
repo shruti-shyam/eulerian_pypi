4199e161dc09ee2d1ca52c9a0661cc0e8988a1cb
Shruti <shrutishyam25@gmail.com>
2019-12-10 16:46:42 +0100

    fixed load video

--- test.py
+++ test.py
@@ -4,15 +4,16 @@ import eulerian_magnification as em
 
 
 def main(args):
-    print(dir(em))
     all_paths = os.listdir(args.input_dir)
-    for vid_path in all_paths:
+
+    for vid_name in all_paths:
+        vid_path = os.path.join(args.input_dir, vid_name)
         vid, fps = em.load_video_float(vid_path)
         em.eulerian_magnification(vid, fps,
-                                      freq_min=50.0 / 60.0,
-                                      freq_max=1.0,
-                                      amplification=50,
-                                      pyramid_levels=3)
+                                  freq_min=50.0 / 60.0,
+                                  freq_max=1.0,
+                                  amplification=50,
+                                  pyramid_levels=3)
 
 
 if __name__ == '__main__':
