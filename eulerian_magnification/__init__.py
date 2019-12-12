57cbf162a5c85bf53a82a92c4dfd209513f1abcf
Shruti <shrutishyam25@gmail.com>
2019-12-12 15:39:58 +0100

    fixed imports

--- eulerian_magnification/__init__.py
+++ eulerian_magnification/__init__.py
@@ -1,4 +1,3 @@
-from .base import eulerian_magnification, show_frequencies
-from .io import load_video_float, save_video
-
-__version__ = '0.22'
+# from .base import eulerian_magnification, show_frequencies
+# from .io import load_video_float, save_video
+# __version__ = '0.22'
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import argparse
+import cv2
 import os
 import eulerian_magnification as em
+from eulerian_magnification import base, io
 
 
 def main(args):
     all_paths = os.listdir(args.input_dir)
     for vid_name in all_paths:
         vid_path = os.path.join(args.input_dir, vid_name)
-        vid, fps = em.load_video_float(vid_path)
-        output_vid = em.eulerian_magnification(vid, fps,
-                                  freq_min=50.0 / 60.0,
-                                  freq_max=1.0,
-                                  amplification=50,
-                                  pyramid_levels=3)
-        em.save_video(output_vid, fps)
+        vid, fps = em.io.load_video_float(vid_path)
+        output_vid = em.base.eulerian_magnification(vid, fps,
+                                                    freq_min=50.0 / 60.0,
+                                                    freq_max=1.0,
+                                                    amplification=50,
+                                                    pyramid_levels=3)
+        em.io.save_video(output_vid, fps)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="eulerian magnification")
